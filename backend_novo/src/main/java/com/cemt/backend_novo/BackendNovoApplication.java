14da07ae2c4adc3f5a4ec5b86cad9f0910e8a0ef
Carlos Enrique Mamani Torrez <carlsenrmt26@gmail.com>
2026-05-12 22:56:55 -0400

    CHORE: ESTRUCTURA INICIAL DEL MONOREPO (BACKEND)

---
+++ backend_novo/src/main/java/com/cemt/backend_novo/BackendNovoApplication.java
@@ -0,0 +1,13 @@
+package com.cemt.backend_novo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendNovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendNovoApplication.class, args);
+	}
+
+}
---
+++ backend_novo/src/test/java/com/cemt/backend_novo/BackendNovoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cemt.backend_novo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendNovoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
